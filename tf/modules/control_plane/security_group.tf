86488b1d604b7ba37e000bcb38f0c6c2f2a64ba2
Danilo Bastos <danilo.bastos@ipnet.cloud>
2021-05-31 18:19:24 -0300

    add role ingress to http port

--- tf/modules/control_plane/security_group.tf
+++ tf/modules/control_plane/security_group.tf
@@ -9,3 +9,14 @@ resource "aws_security_group_rule" "eks_sg_ingress_rule" {
     # Tipo de regra para internal to external utilizar "egress"
     type = "ingress"
 }
+
+resource "aws_security_group_rule" "eks_sg_ingress_rule_http" {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+    # Tipo de regra para internal to external utilizar "egress"
+    type = "ingress"
+}
